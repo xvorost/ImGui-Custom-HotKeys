7380e5639cfbabf59434969d5357d83596298ed9
xvorost <66530241+xvorost@users.noreply.github.com>
2025-05-18 23:57:01 +0200

    Update for newest ImGui

--- Hotkey.h
+++ Hotkey.h
@@ -1,10 +1,15 @@
 #pragma once
 
-#include "imgui/imgui.h"
+#include "../ImGui/imgui.h"
+#include "InputUtil.h"
 
 class KeyBind;
 
 namespace ImGuiCustom
 {
-    void hotkey(const char* label, KeyBind& key, float samelineOffset = 0.0f, const ImVec2& size = { 100.0f, 0.0f }) noexcept;
+    void hotkey(const char* label, KeyBind& keyy, bool* boundBool, float samelineOffset = 0.0f, const ImVec2& size = { 85.0f, 20.0f }) noexcept;
+    bool CheckBindKey(KeyBindToggle key_toggle, bool& func);
+    bool CheckBindKey(KeyBindToggle key_toggle);
+    bool CheckHoldBindKey(KeyBindToggle key_toggle);
+    bool CheckStateBindKey(KeyBindToggle key_toggle);
 }
